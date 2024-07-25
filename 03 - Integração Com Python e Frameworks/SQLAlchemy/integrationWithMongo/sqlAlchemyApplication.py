ab7e16e51815856077bec2c001c865b34c63d16e
Tremendim <luisfernandobeloti@gmail.com>
2024-07-25 11:06:04 -0300

    Estruturação do codigo inicial com a utilização do Framework SQLAlchemy

--- 03 - Integração Com Python e Frameworks/SQLAlchemy/integrationWithMongo/sqlAlchemyApplication.py
+++ 03 - Integração Com Python e Frameworks/SQLAlchemy/integrationWithMongo/sqlAlchemyApplication.py
@@ -1,4 +1,35 @@
-import sqlalchemy
+import sqlalchemy as sqlA
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user_account"
+    #atributos
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    address = relationship(
+        "Address", back_populates="user", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"User(id={self.id}, name={self.name}, fullname={self.fullname}"
+
+
+class Address(Base):
+    __tablename__ = "address"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email_address = Column(String(30), nullable=False)
+    user_id = Column(Integer, ForeignKey("user_account.id"), nullable=False)
+    user = relationship("User", back_populates="address")
+
+    def __repr__(self):
+        return f"Address(id={self.id}, email={self.email_address})"
+
